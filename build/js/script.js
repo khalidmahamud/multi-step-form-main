85d3be7ef3fae58299c34c24cd69c830fc0828f8
Khalid <22-46601-1@student.aiub.edu>
2023-09-15 00:54:23 +0600

    semi-finished step-2

--- build/js/script.js
+++ build/js/script.js
@@ -1,6 +1,51 @@
 const step1 = document.querySelector('#step-1');
 const userInputs = document.querySelectorAll('#step-1 input');
 const nextBtn = document.querySelectorAll('.next-btn');
+const goBackBtn = document.querySelector('.go-back-btn');
+
+let currentStep = document.querySelector('#step-1')
+let currentProgress = document.querySelector(`.step-progress[data-id=${currentStep.id}]`);
+let currentProgressCircle = currentProgress.querySelector('.progress-circle');
+
+let prevStep, prevProgress, prevProgressCircle;
+let nextStep, nextProgress, nextProgressCircle;
+
+function goToNextStep() {
+    currentStep.classList.remove('active');
+    currentProgressCircle.classList.remove('active');
+
+    nextStep = currentStep.nextElementSibling;
+    nextProgress = currentProgress.nextElementSibling;
+    nextProgressCircle = nextProgress.querySelector('.progress-circle');
+    nextStep.classList.add('active');
+    nextProgressCircle.classList.add('active');
+
+    prevStep = currentStep;
+    prevProgress = currentProgress;
+    prevProgressCircle = currentProgressCircle;
+
+    currentStep = nextStep;
+    currentProgress = nextProgress;
+    currentProgressCircle = nextProgressCircle;
+}
+
+function goToPrevStep() {
+    currentStep.classList.remove('active');
+    currentProgressCircle.classList.remove('active');
+
+    prevStep.classList.add('active');
+    prevProgressCircle.classList.add('active');
+    
+    nextStep = currentStep;
+    nextProgress = currentProgress;
+    nextProgressCircle = currentProgressCircle;
+
+    currentStep = prevStep;
+    currentProgress = prevProgress;
+    currentProgressCircle = prevProgress;
+}
+
+
 
 nextBtn.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -24,19 +69,11 @@ nextBtn.forEach(btn => {
         });
     
         if(allInputValid) {
-            const currentStep = btn.parentElement.parentElement;
-            const currentProgress = document.querySelector(`.step-progress[data-id=${currentStep.id}]`);
-            const currentProgressCircle = currentProgress.querySelector('.progress-circle');
-    
-            currentStep.classList.remove('active');
-            currentProgressCircle.classList.remove('active');
-
-    
-            const nextStep = currentStep.nextElementSibling;
-            const nextProgress = currentProgress.nextElementSibling;
-            const nextProgressCircle = nextProgress.querySelector('.progress-circle');
-            nextStep.classList.add('active');
-            nextProgressCircle.classList.add('active');
+            goToNextStep();
         }
     });
+});
+
+goBackBtn.addEventListener('click', () => {
+    goToPrevStep();
 });
